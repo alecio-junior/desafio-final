63ca8a4a6e7586f758954095326e7406008662fa
alecio sales <santosalecio03@gmail.com>
2021-12-06 09:38:17 -0300

    adicionado o winston e mudança de rota

--- api/v1/index.js
+++ api/v1/index.js
@@ -21,7 +21,6 @@ global.logger = winston.createLogger({
 
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/v1/leads", leadsRouter);
 app.use((err, req, res, next) => {
@@ -29,4 +28,4 @@ app.use((err, req, res, next) => {
     res.status(400).send({error: err.message});
 })
 
-app.listen(5500, () => console.log("API started"));
+app.listen(3000, () => console.log("API started"));
