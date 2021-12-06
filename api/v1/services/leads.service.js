d3f801b89402508c53e28c534b5cf670393175f3
alecio sales <santosalecio03@gmail.com>
2021-12-06 09:43:03 -0300

    criação do service das outras rotas

--- api/v1/services/leads.service.js
+++ api/v1/services/leads.service.js
@@ -4,11 +4,26 @@ async function createLeads(leads){
     return await leadsRepository.insertLead(leads);
 }
 
+async function buscarLeads(){
+    return await leadsRepository.buscarLeads();
+}
+
+async function buscarLead(id){
+    return await leadsRepository.buscarLead(id);
+}
+
+async function deleteLead(id){
+   await leadsRepository.deleteLead(id);
+}
+
 async function updateLeads(leads){
-    return await leadsRepository.uptadeLead(leads);
+  return await leadsRepository.updateLeads(leads);
 }
 
 export default{
     createLeads,
+    buscarLeads,
+    buscarLead,
+    deleteLead,
     updateLeads
 }
