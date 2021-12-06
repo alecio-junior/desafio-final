de756763e55cd7d5dbe3b894078b58c806cd34a9
alecio sales <santosalecio03@gmail.com>
2021-12-06 09:42:05 -0300

    adicionado o basic auth, dotenv e adição do repository de admin

--- api/v1/routes/leads.route.js
+++ api/v1/routes/leads.route.js
@@ -1,12 +1,39 @@
 import express from "express";
+import basicAuth from "express-basic-auth";
 import LeadsController from "../controllers/leads.controller.js"
+import AdminRepository from "../repositories/leads.repository.js"
+import dotenv from "dotenv";
+
+dotenv.config();
+AdminRepository.admin()
 
 const router = express.Router();
 
 router.post("/", LeadsController.createLeads);
+
+router.use(basicAuth({
+    authorizer: (username, password) => {
+
+        let acessoUsuario = process.env.USERSIST
+        let acessoSenha = process.env.PASSSIST
+
+        for (let i = 0; i < userAdmin.length; ++i) {
+            if (userAdmin[i].nome == username & userAdmin[i].senha == password) {
+                acessoUsuario = userAdmin[i].nome
+                acessoSenha = userAdmin[i].senha
+            }
+        }
+
+        let userMatches = basicAuth.safeCompare(username, acessoUsuario)
+        let passwordMatches = basicAuth.safeCompare(password, acessoSenha)
+
+        return userMatches & passwordMatches
+    }
+}))
+
 router.get("/", LeadsController.buscarLeads);
-router.get("/:nome", LeadsController.buscarLead);
-router.delete("/:nome", LeadsController.deleteLead);
+router.get("/:id", LeadsController.buscarLead);
+router.delete("/:id", LeadsController.deleteLead);
 router.put("/", LeadsController.updateLeads);
 
 
